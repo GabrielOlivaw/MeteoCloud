b37d9ecf6de30cfa53a2eb38bc7c7b3bdc6daf9e
GabrielOlivaw <36891058+GabrielOlivaw@users.noreply.github.com>
2018-11-01 10:30:31 +0100

    Cambiado título de ventana principal

--- src/meteo_window.py
+++ src/meteo_window.py
@@ -1099,7 +1099,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MeteoCloud"))
         self.labelFiltro.setText(_translate("MainWindow", "Filtrar población"))
         self.pushButtonFiltro.setText(_translate("MainWindow", "Filtrar"))
         __sortingEnabled = self.listWidgetFiltro.isSortingEnabled()
